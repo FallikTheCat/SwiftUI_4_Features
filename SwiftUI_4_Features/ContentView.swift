dc90967a790c23de9681fded0a22253580ca14a0
Baturay Koç <baturay.koc@broadangle.com>
2022-06-07 18:25:05 +0300

    Initial Commit

---
+++ SwiftUI_4_Features/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI_4_Features
+//
+//  Created by Baturay Koc on 7.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_4_Features/SwiftUI_4_FeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_4_FeaturesApp.swift
+//  SwiftUI_4_Features
+//
+//  Created by Baturay Koc on 7.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_4_FeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
